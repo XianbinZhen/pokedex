44212457b37422fe732de01b71cbb03365c0cc50
xianbinzhen <xianbin.zhen001@umb.edu>
2020-07-13 09:15:53 -0400

    Updates

---
+++ precache-manifest.f8b8d92ccdc2d929d0bb28655674127b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3b3746d0490bb77c993f1403748c70",
+    "url": "/XianbinZhen/pokedex/index.html"
+  },
+  {
+    "revision": "dac28154c0794a31b35d",
+    "url": "/XianbinZhen/pokedex/static/css/main.7bafa341.chunk.css"
+  },
+  {
+    "revision": "c629ada326d0c6d6b26a",
+    "url": "/XianbinZhen/pokedex/static/js/2.a9a7d939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/XianbinZhen/pokedex/static/js/2.a9a7d939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac28154c0794a31b35d",
+    "url": "/XianbinZhen/pokedex/static/js/main.d0372914.chunk.js"
+  },
+  {
+    "revision": "b7ca011252e6c2deff58",
+    "url": "/XianbinZhen/pokedex/static/js/runtime-main.da5f977a.js"
+  },
+  {
+    "revision": "b0e3f776a956cb122e65d7b0979b6bc4",
+    "url": "/XianbinZhen/pokedex/static/media/running.b0e3f776.gif"
+  }
+]);
